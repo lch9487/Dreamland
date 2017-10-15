189295e8396bb55ec9a8c4b317628d7690971912
lch9487 <nelson81411@gmail.com>
2017-10-15 15:01:08 +0800

    create-react-app2

--- index.js
+++ index.js
@@ -2,9 +2,5 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", (req, res) => {
-  return res.send("Hello World!");
-});
-
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
